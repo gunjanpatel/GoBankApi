3cecedddc6880caa1c541cc7e36f1d1a032a430d
Gunjan Patel <guapa@vestas.com>
2023-08-16 17:19:10 +0200

    Deposit

--- main.go
+++ main.go
@@ -35,6 +35,46 @@ func statement(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, account.Statement())
 }
 
+func deposit(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	number, err := strconv.ParseFloat(numberqs, 64)
+
+	if err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+		return
+	}
+
+	amount, err := strconv.ParseFloat(amountqs, 64)
+
+	if err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+		return
+	}
+
+	account, ok := accounts[number]
+
+	if !ok {
+		fmt.Fprintf(w, "Account with number %v is not exist", number)
+		return
+	}
+
+	err = account.Deposit(amount)
+
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	fmt.Fprintf(w, account.Statement())
+}
+
 func main() {
 	accounts[1001] = &bank.Account{
 		Customer: bank.Customer{
@@ -46,5 +86,6 @@ func main() {
 	}
 
 	http.HandleFunc("/statement", statement)
+	http.HandleFunc("/deposit", deposit)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
